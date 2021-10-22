912cb9212b3033588309e73a839fe4110e1f131a
roberto <roberto.campedelli@gmail.com>
2021-10-23 00:18:55 +0200

    added the permissions management

--- app/src/main/java/com/example/dont4get/MainActivity.kt
+++ app/src/main/java/com/example/dont4get/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.dont4get
 
+import android.Manifest.permission.*
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.gestures.detectTapGestures
@@ -8,16 +10,20 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.dont4get.ui.theme.Dont4getTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 
-@ExperimentalComposeUiApi
+private val permissions = arrayListOf(RECORD_AUDIO, WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE)
+
 class MainActivity : ComponentActivity() {
+    @ExperimentalPermissionsApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent() {
@@ -31,6 +37,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@ExperimentalPermissionsApi
 @Preview
 @Composable
 fun RecButtonPreview() {
@@ -41,6 +48,7 @@ fun RecButtonPreview() {
 
 enum class ButtonState { Pressed, Released }
 
+@ExperimentalPermissionsApi
 @Composable
 fun RecButton() {
 
@@ -72,6 +80,7 @@ fun myTopAppBar() {
     )
 }
 
+@ExperimentalPermissionsApi
 @Composable
 fun FAB() {
     var buttonState by remember { mutableStateOf(ButtonState.Released) }
@@ -102,11 +111,11 @@ fun FAB() {
                 )
             })
         if (buttonState == ButtonState.Pressed) {
+            StartRec()
             CircularRecProgress()
             LinearRecProgress()
+            Text(text = "$buttonState")
         }
-        Text(text = "$buttonState")
-
     }
 }
 
@@ -130,3 +139,30 @@ fun LinearRecProgress() {
     )
 }
 
+
+@ExperimentalPermissionsApi
+@Composable
+fun StartRec() {
+
+    var context = LocalContext.current
+
+    val permissionsState = rememberMultiplePermissionsState(permissions)
+    when {
+        // If the camera permission is granted, then show screen with the feature enabled
+        permissionsState.allPermissionsGranted -> {
+            //start record
+            Toast.makeText(context, "Camera permission Granted", Toast.LENGTH_SHORT).show()
+        }
+        // If the user denied the permission but a rationale should be shown, or the user sees
+        // the permission for the first time, explain why the feature is needed by the app and allow
+        // the user to be presented with the permission again or to not see the rationale any more.
+        permissionsState.shouldShowRationale ||
+                !permissionsState.permissionRequested -> {
+            LaunchedEffect(permissionsState) {
+                permissionsState.launchMultiplePermissionRequest()
+            }
+            Toast.makeText(context, "GIVE ME THE POWER", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
