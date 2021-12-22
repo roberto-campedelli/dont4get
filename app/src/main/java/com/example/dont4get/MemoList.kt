2231d5c485710269dc7c00a0b5ed01f288c216cb
roberto <roberto.campedelli@gmail.com>
2021-12-23 00:36:50 +0100

    WIP: audio duration/progress

--- app/src/main/java/com/example/dont4get/MemoList.kt
+++ app/src/main/java/com/example/dont4get/MemoList.kt
@@ -4,10 +4,7 @@ import android.media.AudioAttributes
 import android.media.MediaPlayer
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.animateColorAsState
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
@@ -18,6 +15,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -44,6 +42,7 @@ fun MemoCard(memo: Memo, memoViewModel: MemoViewModel) {
 
     val context = LocalContext.current
 
+
     var player = MediaPlayer().apply {
         setAudioAttributes(
             AudioAttributes.Builder()
@@ -55,20 +54,47 @@ fun MemoCard(memo: Memo, memoViewModel: MemoViewModel) {
         prepare()
     }
 
+    val audioProgress by remember {
+        mutableStateOf(Int)
+    }
+    //TODO calculate duration e current position of the media player
+    //val duration = player.duration
+
+    //val audioDuration = LocalTime.of(TimeUnit.MILLISECONDS.toHours(duration),TimeUnit.MILLISECONDS.toHours(duration),TimeUnit.MILLISECONDS.toHours(duration))
+
+/*
+    if(player.isPlaying){
+        while (audioProgress < audioDuration){
+
+        }
+    }
+*/
+
     Card(
-        elevation = 5.dp,
+        elevation = 10.dp,
         modifier = Modifier
             .fillMaxSize()
             .padding(5.dp)
     ) {
-        Row {
-            Column() {
-                Text(text = memo.name, modifier = Modifier.padding(10.dp))
-                Text(text = memo.date, modifier = Modifier.padding(10.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(10.dp)
+            ) {
+                Text(text = memo.name, modifier = Modifier)
+                Text(text = memo.date, modifier = Modifier)
+
             }
 
             PlayPauseButton(player = player)
-            DeleteButton(memo = memo, memoViewModel = memoViewModel, context = context)
+            //Text(text = audioDuration.toString())
+            //Text(text = audioProgress.toString())
+            //DeleteButton(memo = memo, memoViewModel = memoViewModel, context = context)
 
         }
     }
@@ -93,14 +119,59 @@ fun PlayPauseButton(player: MediaPlayer) {
         } else {
             player.start()
         }
-    }) {
+    }, modifier = Modifier.padding(10.dp)) {
         val tint by animateColorAsState(if (checked) Color(0xFF46EC40) else Color(0xFFB0BEC5))
-        if (checked) Icon(
-            Icons.Filled.PlayArrow,
-            contentDescription = "Localized description",
-            tint = tint
-        )
-        else Icon(Icons.Filled.Pause, contentDescription = "Localized description", tint = tint)
+        if (checked) {
+            Icon(
+                Icons.Filled.PlayArrow,
+                contentDescription = "Localized description",
+                tint = tint,
+                modifier = Modifier.size(35.dp)
+            )
+        } else {
+            Icon(
+                Icons.Filled.Pause, contentDescription = "Localized description", tint = tint,
+                modifier = Modifier.size(35.dp)
+            )
+        }
     }
 
 }
+
+//TODO to decide: add or not the control to the speed of the audio
+/*
+@Composable
+fun SpeedUpAudioButton(player: MediaPlayer) {
+
+    var controlEnabled by remember {
+        mutableStateOf(true)
+    }
+    while (player.isPlaying)
+    controlEnabled = true
+
+    TextButton(onClick = {
+        val speed = player.playbackParams.speed
+        player.playbackParams.speed = speed + 0.25F
+    }, enabled = controlEnabled) {
+        Text("faster")
+    }
+}
+
+@Composable
+fun SlowDownAudioButton(player: MediaPlayer) {
+
+    var controlEnabled by remember {
+        mutableStateOf(true)
+    }
+    while (player.isPlaying)
+        controlEnabled = true
+
+    TextButton(onClick = {
+        val speed = player.playbackParams.speed
+        player.playbackParams.speed = speed - 0.25F
+    }, enabled = controlEnabled) {
+        Text("slower")
+    }
+
+}
+*/
