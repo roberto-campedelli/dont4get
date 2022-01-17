352b3782b3256976c7d97b83d52d79f42e5690c5
roberto <roberto.campedelli@gmail.com>
2022-01-17 23:41:28 +0100

    Working on the progress bar

--- app/src/main/java/com/example/dont4get/MemoList.kt
+++ app/src/main/java/com/example/dont4get/MemoList.kt
@@ -1,15 +1,14 @@
 package com.example.dont4get
 
-import android.annotation.SuppressLint
 import android.media.AudioAttributes
 import android.media.MediaPlayer
-import android.util.Log
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Pause
@@ -17,6 +16,7 @@ import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -41,11 +41,6 @@ fun MemoList(memos: List<Memo>, memoViewModel: MemoViewModel) {
 fun MemoCard(memo: Memo, memoViewModel: MemoViewModel) {
 
     val context = LocalContext.current
-    var position by remember {
-        mutableStateOf(0L)
-    }
-
-    var progress by remember { mutableStateOf(0.1f) }
 
     val player = MediaPlayer().apply {
         setAudioAttributes(
@@ -84,8 +79,6 @@ fun MemoCard(memo: Memo, memoViewModel: MemoViewModel) {
 
             DeleteButton(memo = memo, memoViewModel = memoViewModel, context = context)
 
-            //PlayerProgressBar(player = player)
-
         }
     }
 
@@ -99,13 +92,13 @@ fun PlayPauseButton(player: MediaPlayer) {
     var progress by remember {
         mutableStateOf(0F)
     }
-    val duration = player.duration.toFloat()
-    progress = (player.currentPosition.toFloat() - 0) / (duration)
-    Log.i("audioProgress", progress.toString())
 
+    val duration = player.duration.toFloat()
+
+    progress = (player.currentPosition.toFloat() - 0) / (duration)
 
     player.setOnCompletionListener {
-        checked = !checked // finish current activity
+        checked = !checked
     }
 
     IconToggleButton(checked = checked, onCheckedChange = {
@@ -114,9 +107,6 @@ fun PlayPauseButton(player: MediaPlayer) {
             player.pause()
         } else {
             player.start()
-            Log.i("audioProgress1", player.currentPosition.toString())
-            /// todo: call a suspended function to update the current position
-            //GlobalScope.launch { getCurrentPosition(player = player) }
         }
     }, modifier = Modifier.padding(10.dp)) {
         val tint by animateColorAsState(if (checked) Color(0xFF46EC40) else Color(0xFFB0BEC5))
@@ -135,12 +125,24 @@ fun PlayPauseButton(player: MediaPlayer) {
         }
     }
 
-    PlayerProgressBar(progress = progress)
+    val animatedProgress by animateFloatAsState(
+        targetValue = progress,
+        animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec
+    )
+
+    Column() {
+        LinearProgressIndicator(progress = animatedProgress)
+
+        Spacer(Modifier.requiredHeight(30.dp))
+
+        MyIndicator(indicatorProgress = progress, duration = duration.toInt())
+    }
+
+
 }
 
 //TODO - inserisci progress bar e tasti per andare avanti e indietro!
 
-@SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun PlayerProgressBar(progress: Float) {
 
@@ -150,4 +152,20 @@ fun PlayerProgressBar(progress: Float) {
     )
     LinearProgressIndicator(progress = animatedProgress)
     Spacer(Modifier.requiredHeight(30.dp))
+}
+
+@Composable
+fun MyIndicator(indicatorProgress: Float, duration: Int) {
+    var progress by remember { mutableStateOf(0f) }
+    val progressAnimDuration = duration
+    val progressAnimation by animateFloatAsState(
+        targetValue = indicatorProgress
+    )
+    LinearProgressIndicator(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(20.dp)), // Rounded edges
+        progress = progressAnimation
+    )
+
 }
